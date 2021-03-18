cabf023510d22428614447014211aa596578ec2f
Maxi-chelsea <maxi19950049@gmail.com>
2021-03-18 12:55:34 +0000

    [Post1]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,14 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book.id)
       flash[:notice] = "Book was successfully created."
