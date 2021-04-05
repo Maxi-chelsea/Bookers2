ff4da9ab6fa7764638cbf758bc09087bfe59e7c0
Maxi-chelsea <maxi19950049@gmail.com>
2021-04-05 13:02:49 +0000

    [Update2]

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,30 @@ class User < ApplicationRecord
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+
+  def follow(user_id)
+    unless self.id == user_id.to_i
+      relationships.create(followed_id: user_id)
+    end
+  end
+
+  def unfollow(user_id)
+    unless self.id == user_id.to_i
+      relationships.find_by(followed_id: user_id).destroy
+    end
+  end
+
+  def following?(user)
+    unless self.id == user.id
+      followings.include?(user)
+    end
+  end
+
 end
